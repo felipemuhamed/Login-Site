48577363ccac994a3c9729403c4140a8858b7d0d
Felipe Muhamed <39491086+muhamedcpqd@users.noreply.github.com>
2018-06-08 16:36:50 -0300

    Buton effect

--- src/Form.js
+++ src/Form.js
@@ -15,30 +15,32 @@ export default class Form extends Component {
   handleButtonClick = e => {
     e.preventDefault();
 
-    if (this.state.user.length !== 0) {
-      console.log(this.state.user);
-    } else {
+    if (this.state.user.length !== 0 && this.state.password.length !== 0) {
+      document.getElementById("buttonLogin").style.animation =
+        "jello-horizontal cubic-bezier(0.47, 0, 0.745, 0.715) 1s 4 both";
+
+      setTimeout(() => {
+        if (this.state.user === "femuhamed@hotmail.com") {
+          console.log("LOGGED");
+        } else {
+          console.log("Error in LOGIN");
+        }
+        document.getElementById("buttonLogin").style.animation = "";
+      }, 3000);
+    } else if (this.state.user.length === 0) {
       document.getElementById("userNameInput").style.animation =
         "shake-horizontal 0.8s cubic-bezier(0.455, 0.03, 0.515, 0.955) both";
 
       setTimeout(function() {
         document.getElementById("userNameInput").style.animation = "";
       }, 800);
-
-      console.log("Please, insert your username");
-    }
-
-    if (this.state.password.length !== 0) {
-      console.log(this.state.password);
-    } else {
+    } else if (this.state.password.length === 0) {
       document.getElementById("PasswordInput").style.animation =
         "shake-horizontal 0.8s cubic-bezier(0.455, 0.03, 0.515, 0.955) both";
 
       setTimeout(function() {
         document.getElementById("PasswordInput").style.animation = "";
       }, 800);
-
-      console.log("Please, insert your Password");
     }
   };
 
@@ -75,13 +77,16 @@ export default class Form extends Component {
               onChange={this.updatePassword}
             />
           </div>
-          <input
+          <button
+            id="buttonLogin"
             type="submit"
             name="submit"
             value="LOGIN"
             className="btn-login"
             onClick={this.handleButtonClick}
-          />
+          >
+            LOGIN
+          </button>
           <br />
           <a>Esqueceu a senha?</a>
         </form>
